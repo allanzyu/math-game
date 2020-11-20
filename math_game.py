2f9d066f2c5b91d9457aa113be7a1be367444835
allanzyu <allanzyu@gmail.com>
2020-11-19 19:02:44 -0800

    Update math_game.py

--- math_game.py
+++ math_game.py
@@ -9,6 +9,92 @@ Midterm - Math Game
 
 """
 
-class game:
+import tkinter as tk
+import random
+
+class GameGui:
     def __init__(self):
-        pass
+        # create root window widget
+        self.root = tk.Tk()
+        self.root.title('Math Game!')
+
+        # Initialize label widget to display welcome message
+        self.lbl_string = tk.StringVar()
+        self.lbl_string.set('Welcome to Math Game!')
+        self.lbl_out = tk.Label(self.root, textvariable = self.lbl_string)
+
+        # Initialize buttons
+        self.btn_addition = tk.Button(text = '+')
+
+        # pack dat shit
+        self.lbl_out.pack()
+        self.btn_addition.pack()
+
+        tk.mainloop()
+
+# Addition question object
+#   Generates a random addition problem of 2 int addends
+#   between 1 and 10 when instantiated
+# 2 Attributes:
+#   __question
+#   __solution
+# 1 Method:
+#   new_prob
+class Addition:
+    def __init__(self):
+        self.__question = 'foo'
+        self.__solution = 0
+        self.new_prob()
+
+    @property
+    def question(self):
+        return self.__question
+
+    @question.setter
+    def question(self,question):
+        self.__question = question
+
+    @property
+    def solution(self):
+        return self.__solution
+
+    @solution.setter
+    def solution(self, solution):
+        self.__solution = solution
+
+    def new_prob(self):
+        A = random.randint(1,10)
+        B = random.randint(1,10)
+        self.question = (str(A)+" + "+str(B)+" = ?") #generates string version of problem
+        self.solution = A + B #generates correct answer
+
+# Subtraction question object is a subclass of Addition
+# Generates a random subtraction problem with int solution > 0
+class Subtraction(Addition):
+    def new_prob(self):
+        A = random.randint(1,10)
+        B = random.randint(1,10)
+        C = A + B
+        self.question = (str(C)+" - "+str(A)+" = ?") #generates string version of problem
+        self.solution = B #generates correct answer
+
+# Multiplication question object is a subclass of Addition
+# Generates a random multiplication problem with int product > 0
+class Multiplication(Addition):
+    def new_prob(self):
+        A = random.randint(1,10)
+        B = random.randint(1,10)
+        self.question = (str(A)+" x "+str(B)+" = ?") #generates string version of problem
+        self.solution = A*B #generates correct answer
+
+# Division question object is a subclass of Addition
+# Generates a random division problem with 
+class Division(Addition):
+    def new_prob(self):
+        A = random.randint(1,10)
+        B = random.randint(1,10)
+        C = A*B
+        self.question = (str(C)+" / "+str(A)+" = ?") #generates string version of problem
+        self.solution = B #generates correct answer
+
+test = GameGui()
